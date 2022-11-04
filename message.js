e74280e71aad8ac4c15cab29a0c190db7371efb9
rodney24kd <rodney24kd@gmail.com>
2022-11-04 15:25:29 -0500

    Added Response To Joke

--- message.js
+++ message.js
@@ -1,10 +1,14 @@
 const knockknockJokes = ['Who\’s there? Nobel. Nobel who? Nobel…that\’s why I knocked!', 'Who\’s there? Luke. Luke who? Luke through the peep hole and find out.', 'Who\’s there? Cow says. Cow says who? No, a cow says mooooo!' ];
 
+const statements = ['Not so funny...', 'Do not quit your day job...', 'rofl'];
+
 const randyNum = Math.floor(Math.random()*3);
 
-const randyJoke = arr => {
+const randyJoke = (arr,arr2) => {
     let joke = arr[randyNum];
-    return console.log(joke);
+    console.log(joke);
+    let response = arr2[randyNum];
+    console.log(response);
 }
 
-randyJoke(knockknockJokes);
+randyJoke(knockknockJokes, statements);
